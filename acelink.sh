b2876a8d0fc13d0272829e1da7addb7cc74b6264
Amar <amar@Macmini.lan>
2025-01-11 18:04:46 -0500

    clean up

--- acelink.sh
+++ acelink.sh
@@ -8,5 +8,3 @@ else
     echo $contentId
     vlc http://127.0.0.1:6878/ace/getstream?content_id=$contentId
 fi
-
-# http://127.0.0.1:6878/ace/getstream?content_id=fc5089d8e1519872fdf951779ccbca913acc9bce
